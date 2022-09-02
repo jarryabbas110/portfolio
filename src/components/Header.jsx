54c0cc2d35a82d0a41d46a08c5da046f024d4f46
jarryabbas110 <jarryabbas60@gmail.com>
2022-09-02 19:13:30 +0500

    updates 2.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,36 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../assets/me.png'
 import Social from './Socials'
-import Nav from './Nav'
+
 
 const Header = () => {
   return (
+
+
+    
    <header>
-    <div className="container header__container">
+
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+    
+    <div className="banner">
+         <div className="banner-text">
+            <h5>Hello I'm</h5>
+            <h1>Jarry Abbas.</h1>
+            <h3>Frontend Developer | DevOps Engineer | Ui & Ux Designer</h3>
+            <CTA />
+            <hr />
+            {/* <ul className="social">
+            
+            </ul> */}
+         </div>
+       
+      </div>
+      <Social />
+
+
+    {/* <div className="container header__container">
       <h5>Hello I'm</h5>
       <h1>Jarry Abbas Naqvi</h1>
       <h5 className="text-light">Frontend Developer</h5>
@@ -17,14 +41,11 @@ const Header = () => {
 
       <div className="me">
         <img src={ME} alt="me" />
-      </div>
+      </div> */}
   
-      {/* <a href="#contact" className='scroll__down'>Scroll Down</a>  1:04*/}
-     
 
-
-
-    </div>
+      <a href="#about" className='scroll__down'>Scroll Down</a>
+      {/* </div> */}
    </header>
   )
 }
